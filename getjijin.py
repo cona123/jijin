bc94f09a1c092c1990fe099d65c0259d3f0a0500
cona123 <chaoyue19890625@gmail.com>
2020-09-11 23:04:40 +0800

    增加成本和总金额计算，计算出收益的绝对值

--- getjijin.py
+++ getjijin.py
@@ -1,7 +1,11 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+
 def get_html(url):
+    global fenshu
+    global zongjine
+    global chengben
     headers = {
         'User-Agent':'Mozilla/5.0(Macintosh; Intel Mac OS X 10_11_4)\
         AppleWebKit/537.36(KHTML, like Gecko) Chrome/52 .0.2743. 116 Safari/537.36'
@@ -9,26 +13,31 @@ def get_html(url):
     }     #模拟浏览器访问
     response = requests.get(url,headers = headers)       #请求访问网站
     html = response.text       #获取网页源码
-    print(html)
     serobject =re.findall(r"<td>(\d+-\d+-\d+)</td><td class='tor bold'>(\S+)</td><td class='tor bold'>(\S+)</td>",html, re.M)
-    print(serobject)
     time = ""
     val = ""
     for i in serobject:
-        print ("时间：%s   值：%s" % (i[0], i[2]))
         time = i[0]
         val = i[2]
+
+        
 
     #match=reg.search(str(html))
     #print(reg)
     if time!= "":
-        print("时间2：%s   值2：%s" % (i[0], i[2]))
+        print("时间2：%s   值2：%s" % (time, val))
+        zongjine = fenshu*float(val) + 20000
+        fenshu = 20000/float(val)+fenshu
+        chengben = chengben + 20000
     return html                #返回网页源码
     
     
 
-begintime = 2017 
-endtime =  2020
+begintime = 2009
+endtime =  2016
+chengben = 0.0
+zongjine = 0.0
+fenshu = 0.0
 for i in range(begintime,endtime):
     begintime1 = str(i-1)+"-12-31"
     endtime1 = str(i)+"-01-01"
@@ -39,3 +48,5 @@ for i in range(begintime,endtime):
     endtime1 = str(i)+"-07-01"
     msg =  aaa %("100032",begintime1,endtime1)
     soup = BeautifulSoup(get_html(msg), 'lxml')   #初始化BeautifulSoup库,并设置解析器
+    print("zongjine：%s   chengben：%s" % (zongjine, chengben))
+
