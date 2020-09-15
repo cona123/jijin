3799079124bcf7c7685ea9a939f12608a3bad483
cona123 <chaoyue19890625@gmail.com>
2020-09-15 21:40:53 +0800

    增加三个基金的时间平均策略和投资收益计算，暂时over

--- getjijin.py
+++ getjijin.py
@@ -33,6 +33,7 @@ chengben = 0.0
 zongjine = 0.0
 fenshu = 0.0
 fenshu1 = 0.0
+fenshu2 = 0.0
 for i in range(begintime,endtime):
     begintime1 = str(i-1)+"-12-31"
     endtime1 = str(i)+"-01-01"
@@ -43,9 +44,14 @@ for i in range(begintime,endtime):
     aaa = 'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code=%s&sdate=%s&edate=%s&per=40&page=1'
     msg =  aaa %("000834",begintime1,endtime1)
     val1 = get_html(msg)#, 'lxml')   #初始化BeautifulSoup库,并设置解析器
-    zongjine = fenshu*float(val) +fenshu1*float(val1) + 20000
-    fenshu = zongjine*0.5/float(val)
-    fenshu1 = zongjine*0.5/float(val1)
+    aaa = 'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code=%s&sdate=%s&edate=%s&per=40&page=1'
+    msg =  aaa %("217011",begintime1,endtime1)
+    #val = BeautifulSoup(get_html(msg), 'lxml')   #初始化BeautifulSoup库,并设置解析器
+    val2 = get_html(msg)
+    zongjine = fenshu*float(val) +fenshu1*float(val1) +fenshu2*float(val2)+ 20000
+    fenshu = zongjine*0.35/float(val)
+    fenshu1 = zongjine*0.35/float(val1)
+    fenshu2 = zongjine*0.3/float(val2)
     chengben = chengben + 20000
     begintime1 = str(i)+"-06-30"
     endtime1 = str(i)+"-07-01"
@@ -54,9 +60,13 @@ for i in range(begintime,endtime):
     aaa = 'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code=%s&sdate=%s&edate=%s&per=40&page=1'
     msg =  aaa %("000834",begintime1,endtime1)
     val2 = get_html(msg)#, 'lxml')   #初始化BeautifulSoup库,并设置解析器    
-    zongjine = fenshu*float(val) +fenshu1*float(val1) + 20000
-    fenshu = zongjine*0.5/float(val)
-    fenshu1 = zongjine*0.5/float(val1)
+    aaa = 'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code=%s&sdate=%s&edate=%s&per=40&page=1'
+    msg =  aaa %("217011",begintime1,endtime1)
+    val2 = get_html(msg)
+    zongjine = fenshu*float(val) +fenshu1*float(val1) +fenshu2*float(val2)+ 20000
+    fenshu = zongjine*0.35/float(val)
+    fenshu1 = zongjine*0.35/float(val1)
+    fenshu2 = zongjine*0.3/float(val2)
     chengben = chengben + 20000
     print("zongjine：%s   chengben：%s" % (zongjine, chengben))
 
